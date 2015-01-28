8eca973e21e49567f9f63f0db452908dafc69732
Julian Mazzitelli <mazzitell.julian@gmail.com>
2015-01-28 01:13:55 -0500

    Moved binaries to ./bin

--- remvocals.c
+++ remvocals.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 int main (int argc, char **argv ) {
+
+	// Print number of command line arguments
 	printf("%d command line arguments. They are: \n", argc);
+	// Print value of command line arguments
 	int i;
 	for(i = 0; i < argc; i++) {
 		printf("%d: %s\n",i,argv[i]);
+	}
+
+	// Check for proper number of command line arguments
+	if (argc != 3){
+		printf("Incorrect progam call, exiting...bye...\n");
+		return 1;
 	}
 
 	return 0;
